18e613ddaf324f499ea1ba399c90a374d59950db
ArthuRrezen <99223138+ArthuRrezen@users.noreply.github.com>
2022-05-15 21:25:07 -0300

    Add files via upload

---
+++ Aula2203/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula2203
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int produto = int.Parse(textBox1.Text);
+            double resultado;
+            resultado = produto * 0.10;
+            MessageBox.Show(resultado.ToString());
+
+        }
+    }
+}
